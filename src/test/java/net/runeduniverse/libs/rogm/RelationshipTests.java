63eb784989d679978373e844b6f616a89ccd7e46
Rene Brandstetter <renebrandy2@gmail.com>
2020-07-06 19:29:55 +0200

    Refactoring: Split cration fo FilterNode and FilterRelation int their own Methodds and deleted old Comments

--- src/test/java/net/runeduniverse/libs/rogm/RelationshipTests.java
+++ src/test/java/net/runeduniverse/libs/rogm/RelationshipTests.java
@@ -30,6 +30,7 @@ import net.runeduniverse.libs.rogm.parser.Parser;
 import net.runeduniverse.libs.rogm.querying.FilterNode;
 import net.runeduniverse.libs.rogm.querying.FilterRelation;
 import net.runeduniverse.libs.rogm.querying.IFNode;
+import net.runeduniverse.libs.rogm.querying.IFRelation;
 import net.runeduniverse.libs.rogm.querying.IFilter;
 
 public class RelationshipTests {
@@ -57,10 +58,12 @@ public class RelationshipTests {
 	public void testUsingActorClass() throws Exception {
 		System.out.println(cypher.buildQuery(giveFilterNodeOrRelation(Actor.class, false), parser));
 	}
+
 	@Test
 	public void testUsingGameClass() throws Exception {
 		System.out.println(cypher.buildQuery(giveFilterNodeOrRelation(Game.class, false), parser));
 	}
+
 	@Test
 	public void testUsingCompanyClass() throws Exception {
 		System.out.println(cypher.buildQuery(giveFilterNodeOrRelation(Company.class, false), parser));
@@ -78,73 +81,11 @@ public class RelationshipTests {
 
 		// Create FilterNode
 		if (checkIfClassIsNode(clazz)) {
-			FilterNode fn = new FilterNode().setReturned(true).setOptional(isChild);
-			classMap.put(clazz, fn);
-			
-			List<String> labels = new ArrayList<String>();
-			getLabelsForClass(clazz, labels);
-			fn.addLabels(labels);
-			
-			Field[] f = clazz.getDeclaredFields();
-			for (int i = 0; i < f.length; i++) {
-				Field ff = f[i];
-				if (ff.isAnnotationPresent(Relationship.class)) {
-					Class<?> ffClazz = ff.getType();
-					Relationship r = ff.getAnnotation(Relationship.class);
-					String label = r.label().isEmpty() ? ff.getName() : r.label();
-					if (isOfTypeCollection(ffClazz)) {
-						/*
-						 * System.out.println("TEST1:"+ff); System.out.println("TEST2:"+ffClazz);
-						 * System.out.println("TEST3:"+ff.getGenericType());
-						 * System.out.println("TEST4:"+ff.getGenericType().getTypeName());
-						 * System.out.println("TEST5:"+((ParameterizedType)
-						 * ff.getGenericType()).getRawType());
-						 */
-						ffClazz = getClassFromCollectionField(ff);
-					}
-					if (ffClazz.isAnnotationPresent(RelationshipEntity.class)) {
-						FilterRelation fr = (FilterRelation) giveFilterNodeOrRelation(ffClazz, true);
-						if (fr.getLabels().isEmpty())
-							fr.addLabel(label);
-						fn.addRelation(fr);
-					} else {
-						FilterRelation fr = new FilterRelation(r.direction()).addLabel(label).setReturned(true).setOptional(true);
-						fn.addRelation(fr, (IFNode) giveFilterNodeOrRelation(ffClazz, true));
-					}
-				}
-			}
-			return fn;
+			return createFilterNode(clazz, isChild);
 		} else {
 			// Create FilterRelation
 			if (checkIfClassIsRelationshipEntity(clazz)) {
-				Boolean startNode = false, endNode = false;
-				FilterRelation fr = new FilterRelation().setReturned(true).setOptional(isChild);
-				classMap.put(clazz, fr);
-				
-				RelationshipEntity re = clazz.getAnnotation(RelationshipEntity.class);
-				fr.setDirection(re.direction());
-				if (!re.label().isEmpty())
-					fr.addLabel(re.label());
-				
-				Field[] fields = clazz.getDeclaredFields();
-				for (Field field : fields) {
-					Class<?> fieldClass = field.getType();
-					if (!(field.isAnnotationPresent(StartNode.class) || field.isAnnotationPresent(EndNode.class)))
-						continue;
-					if (isOfTypeCollection(fieldClass))
-						throw new Exception("A Collection inside RelationshipEntity is not allowed!!!");
-					if (field.isAnnotationPresent(StartNode.class)) {
-						startNode = true;
-						fr.setStart((IFNode) giveFilterNodeOrRelation(fieldClass, true));
-					} else if (field.isAnnotationPresent(EndNode.class)) {
-						endNode = true;
-						fr.setTarget((IFNode) giveFilterNodeOrRelation(fieldClass, true));
-					}
-				}
-				if (!(startNode && endNode))
-					throw new Exception(
-							"A RelationshipEntity needs a Field with the StartNode Annotation and a Field with the EndNode Annotation!!!");
-				return fr;
+				return createFilterRelation(clazz, isChild);
 			}
 			// I have no idea what you gave me so go away!
 			else {
@@ -153,26 +94,72 @@ public class RelationshipTests {
 		}
 	}
 
+	private IFNode createFilterNode(Class<?> clazz, boolean isChild) throws Exception {
+		FilterNode fn = new FilterNode().setReturned(true).setOptional(isChild);
+		classMap.put(clazz, fn);
+
+		List<String> labels = new ArrayList<String>();
+		getLabelsForClass(clazz, labels);
+		fn.addLabels(labels);
+
+		Field[] f = clazz.getDeclaredFields();
+		for (int i = 0; i < f.length; i++) {
+			Field ff = f[i];
+			if (ff.isAnnotationPresent(Relationship.class)) {
+				Class<?> ffClazz = ff.getType();
+				Relationship r = ff.getAnnotation(Relationship.class);
+				String label = r.label().isEmpty() ? ff.getName() : r.label();
+				if (isOfTypeCollection(ffClazz)) {
+					ffClazz = getClassFromCollectionField(ff);
+				}
+				if (ffClazz.isAnnotationPresent(RelationshipEntity.class)) {
+					FilterRelation fr = (FilterRelation) giveFilterNodeOrRelation(ffClazz, true);
+					if (fr.getLabels().isEmpty())
+						fr.addLabel(label);
+					fn.addRelation(fr);
+				} else {
+					FilterRelation fr = new FilterRelation(r.direction()).addLabel(label).setReturned(true)
+							.setOptional(true);
+					fn.addRelation(fr, (IFNode) giveFilterNodeOrRelation(ffClazz, true));
+				}
+			}
+		}
+		return fn;
+	}
+
+	private IFRelation createFilterRelation(Class<?> clazz, boolean isChild) throws Exception {
+		Boolean startNode = false, endNode = false;
+		FilterRelation fr = new FilterRelation().setReturned(true).setOptional(isChild);
+		classMap.put(clazz, fr);
+
+		RelationshipEntity re = clazz.getAnnotation(RelationshipEntity.class);
+		fr.setDirection(re.direction());
+		if (!re.label().isEmpty())
+			fr.addLabel(re.label());
+
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			Class<?> fieldClass = field.getType();
+			if (!(field.isAnnotationPresent(StartNode.class) || field.isAnnotationPresent(EndNode.class)))
+				continue;
+			if (isOfTypeCollection(fieldClass))
+				throw new Exception("A Collection inside RelationshipEntity is not allowed!!!");
+			if (field.isAnnotationPresent(StartNode.class)) {
+				startNode = true;
+				fr.setStart((IFNode) giveFilterNodeOrRelation(fieldClass, true));
+			} else if (field.isAnnotationPresent(EndNode.class)) {
+				endNode = true;
+				fr.setTarget((IFNode) giveFilterNodeOrRelation(fieldClass, true));
+			}
+		}
+		if (!(startNode && endNode))
+			throw new Exception(
+					"A RelationshipEntity needs a Field with the StartNode Annotation and a Field with the EndNode Annotation!!!");
+		return fr;
+	}
+
 	private Class<?> getClassFromCollectionField(Field collectionClass) throws ClassNotFoundException {
-		ParameterizedType type = (ParameterizedType) collectionClass.getGenericType();
-		// System.out.println("TTEST1:"+type);
-		// System.out.println("TTEST2:"+Class.forName("net.runeduniverse.libs.rogm.model.Actor"));
-		Type[] typeArgs = type.getActualTypeArguments();
-		// System.out.println("TTEST3:"+typeArgs);
-		/*
-		 * for (Type type2 : typeArgs) { System.out.println("TTEST4:"+type2); }
-		 */
-		// System.out.println("TTEST5:"+typeArgs[0]);
-		Type t = typeArgs[0];
-		// System.out.println("TTEST6:"+t);
-		Class<?> c = (Class<?>) t;
-		// System.out.println("TTEST7:"+c);
-		/*
-		 * ParameterizedType type = (ParameterizedType)
-		 * collectionClass.getGenericType(); return (Class<?>)
-		 * type.getActualTypeArguments()[0];
-		 */
-		return c;
+		return (Class<?>) ((Type[]) ((ParameterizedType) collectionClass.getGenericType()).getActualTypeArguments())[0];
 	}
 
 	private Boolean isOfTypeCollection(Class<?> maybeCollection) {
