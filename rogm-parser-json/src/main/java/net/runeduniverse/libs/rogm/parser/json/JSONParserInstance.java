fb450c8d1415802b97f587bbe0a36bcb0390cede
Pl4yingNight <pl4yingnight@gmail.com>
2021-01-02 18:21:14 +0100

    rogm version 2.0 (new versioning <main version>.<patch version>.<module version>

---
+++ rogm-parser-json/src/main/java/net/runeduniverse/libs/rogm/parser/json/JSONParserInstance.java
@@ -0,0 +1,33 @@
+package net.runeduniverse.libs.rogm.parser.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import net.runeduniverse.libs.rogm.parser.Parser.Instance;
+
+@RequiredArgsConstructor
+class JSONParserInstance implements Instance {
+	private final ObjectMapper mapper;
+
+	@Override
+	public String serialize(Object object) throws JsonProcessingException {
+		return mapper.writeValueAsString(object);
+	}
+
+	@Override
+	public <T> T deserialize(Class<T> clazz, String value)
+			throws JsonMappingException, JsonProcessingException, InstantiationException, IllegalAccessException {
+		if (value == null)
+			return clazz.newInstance();
+		return mapper.readValue(value, clazz);
+	}
+
+	@Override
+	public <T> T deserialize(T obj, String value) throws JsonMappingException, JsonProcessingException {
+		if (value == null)
+			return obj;
+		return this.mapper.readerForUpdating(obj).readValue(value);
+	}
+}
