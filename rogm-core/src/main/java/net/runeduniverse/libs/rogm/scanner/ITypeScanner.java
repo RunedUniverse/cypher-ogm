ec5b0e520ff85f94a5cd2b09a0daa731567d84f8
Pl4yingNight <pl4yingnight@gmail.com>
2021-04-15 20:26:30 +0200

    moved package scanner to https://github.com/RunedUniverse/utils

--- rogm-core/src/main/java/net/runeduniverse/libs/rogm/scanner/ITypeScanner.java
+++
@@ -1,5 +0,0 @@
-package net.runeduniverse.libs.rogm.scanner;
-
-public interface ITypeScanner {
-	void scan(Class<?> type, ClassLoader loader, String pkg) throws Exception;
-}
--- rogm-core/src/main/java/net/runeduniverse/libs/rogm/scanner/ScanOrder.java
+++
@@ -1,5 +0,0 @@
-package net.runeduniverse.libs.rogm.scanner;
-
-public enum ScanOrder {
-	FIRST, LAST, ALL
-}
