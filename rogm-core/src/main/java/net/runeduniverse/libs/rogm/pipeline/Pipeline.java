44c7cb7d543d89309d85899c94ff6a652ce8c6e6
Pl4yingNight <pl4yingnight@gmail.com>
2021-07-29 11:16:49 +0200

    cleanup

--- rogm-core/src/main/java/net/runeduniverse/libs/rogm/pipeline/Pipeline.java
+++ rogm-core/src/main/java/net/runeduniverse/libs/rogm/pipeline/Pipeline.java
@@ -1,7 +1,5 @@
 package net.runeduniverse.libs.rogm.pipeline;
 
-import java.util.Map;
-
 import net.runeduniverse.libs.rogm.Configuration;
 import net.runeduniverse.libs.rogm.error.ScannerException;
 import net.runeduniverse.libs.rogm.pattern.Archive;
@@ -10,26 +8,23 @@ public class Pipeline {
 	private final Configuration cnf;
 	private final Archive archive;
 	private final IRouter router;
-	private final Map<String, IFactory> factories;
+	
+	private Assembler assembler = null;
 
-	protected Pipeline(final Configuration cnf, final IRouter router, final Map<String, IFactory> factories) {
+	protected Pipeline(final Configuration cnf, final IRouter router) {
 		this.cnf = cnf;
 		this.archive = new Archive(this.cnf);
 		this.router = router;
 		router.setup(this);
-		this.factories = factories;
 	}
 	
 	public Pipeline setup() throws ScannerException {
 		this.archive.applyConfig();
+		this.assembler = new Assembler(this.archive);
 		return this;
 	}
 
 	public boolean process(Request<?> request) {
 		return true;// false => aborted
-	}
-
-	public IFactory getFactory(String pipe) {
-		return this.factories.get(pipe);
 	}
 }
