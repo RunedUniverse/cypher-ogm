3913d1b293da4c0cae39ddfad284a1416a88cbd0
Pl4yingNight <pl4yingnight@gmail.com>
2021-10-13 21:47:14 +0200

    fixed bug in IValidatable where correct method never got called doe to override with Object param

--- rogm-core/src/main/java/net/runeduniverse/libs/rogm/pattern/IValidatable.java
+++ rogm-core/src/main/java/net/runeduniverse/libs/rogm/pattern/IValidatable.java
@@ -3,15 +3,19 @@ package net.runeduniverse.libs.rogm.pattern;
 import java.util.Collection;
 
 public interface IValidatable {
+	
 	void validate() throws Exception;
 
-	public static void validate(Object obj) throws Exception {
-		if (obj instanceof IValidatable)
-			((IValidatable) obj).validate();
-	}
+	public static void validate(Object... values) throws Exception {
+		if (values == null)
+			return;
 
-	public static void validate(Collection<Object> col) throws Exception {
-		for (Object obj : col)
-			IValidatable.validate(obj);
+		for (Object obj : values) {
+			if (obj instanceof IValidatable)
+				((IValidatable) obj).validate();
+			if (obj instanceof Collection<?>)
+				for (Object element : (Collection<?>) obj)
+					IValidatable.validate(element);
+		}
 	}
 }
