6d57fa510b08db8ba7e5bcec26c468c289d6491c
Pl4yingNight <pl4yingnight@gmail.com>
2021-12-05 16:37:00 +0100

    fixed licese header & pom.xml

--- rogm-core/src/main/java/net/runeduniverse/libs/rogm/buffer/BasicBufferLayers.java
+++ rogm-core/src/main/java/net/runeduniverse/libs/rogm/buffer/BasicBufferLayers.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.runeduniverse.libs.rogm.buffer;
 
 import java.util.ArrayList;
